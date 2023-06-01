16bae4be8a5789eceedaab18cb6d67dc25914ba7
Eric Lin <93724436+Rinciel@users.noreply.github.com>
2023-06-01 17:58:22 -0400

    Start news api

--- news.py
+++ news.py
@@ -1,24 +1,33 @@
 """This file processes the news data and saves it to a JSON file."""
 
-# API: https://newsapi.org/docs/get-started
+# API: https://www.marketaux.com/documentation
 
 import os
+import requests
+import json
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
 
 API_TOKEN = None
-COMPANIES = None
 
-def news_init(companies):
+def news_init():
     """Initialize the news data."""
-    global COMPANIES
-    COMPANIES = companies
 
     load_dotenv()
     global API_TOKEN
     API_TOKEN = os.getenv("MARKETAUX_API_TOKEN")
 
-def get_news(company_name):
+def get_news(ticker):
     """Return the news data for said company."""
-    pass
+
+    if (API_TOKEN is None):
+        news_init()
+    # build the request
+    url = "https://api.marketaux.com/v1/news/all?" + \
+          "symbols=" + ticker + \
+          "&filter_entities=true" + \
+          "&api_token=" + API_TOKEN
     
+    print(url)
+    # send the request
+    # response = requests.get(url)
