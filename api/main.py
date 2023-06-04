b255058b647a7f8b1358c7c045ff6018e07694a2
Eric Lin <93724436+Rinciel@users.noreply.github.com>
2023-06-04 16:22:54 -0400

    Set up flask scheduler

--- api/main.py
+++ api/main.py
@@ -2,10 +2,24 @@
 
 import os
 import configparser
+import news
+import stock
 from flask import Flask, render_template
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
+scheduler = BackgroundScheduler(daemon=True)
 
+stock = stock.Stock()
+news = news.News(stock.get_companies_tickers())
+
+# ==============================================================================
+def update_data():
+    """Update the stock data."""
+    stock.save_previous_closings()
+    stock.save_current_prices()
+    news.save_news()
+# ==============================================================================
 @app.route("/")
 def index():
     """Return the index page."""
@@ -27,7 +41,7 @@ def main():
     if not os.path.exists(".env"):
         # read the API key from config.ini
         config = configparser.ConfigParser()
-        config.read("config.ini")
+        config.read("api/config.ini")
         api_token = config["API"]["API_TOKEN"]
         # create a .env file and write the API token to it
         with open(".env", "w", encoding="utf-8") as file:
@@ -36,4 +50,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+    scheduler.add_job(func=update_data, trigger="interval", seconds=30)
+    
+    scheduler.start()
     app.run()
