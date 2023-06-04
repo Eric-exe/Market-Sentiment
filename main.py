409a7d023772ca6e9bacc86346174ec715a67a27
Eric Lin <93724436+Rinciel@users.noreply.github.com>
2023-06-03 23:04:01 -0400

    Add Flask

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
 """Main file for the project."""
+
+import os
+import configparser
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    """Return the index page."""
+    return render_template("index.html")
+
+@app.route("/styles.css")
+def styles():
+    """Return the styles.css file."""
+    return app.send_static_file("styles.css")
+
+@app.route("/script.js")
+def scripts():
+    """Return the scrips.js file."""
+    return app.send_static_file("script.js")
+# ==============================================================================
+def main():
+    """Main function."""
+    # check if .env exists
+    if not os.path.exists(".env"):
+        # read the API key from config.ini
+        config = configparser.ConfigParser()
+        config.read("config.ini")
+        api_token = config["API"]["API_TOKEN"]
+        # create a .env file and write the API token to it
+        with open(".env", "w", encoding="utf-8") as file:
+            file.write("MARKETAUX_API_TOKEN=" + api_token)
+
+
+if __name__ == "__main__":
+    main()
+    app.run()
